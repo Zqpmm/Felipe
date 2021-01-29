e7e25a765659d4115f2429d10dd631c4f6d3da56
Zqpmm <53921838+Zqpmm@users.noreply.github.com>
2021-01-29 02:14:23 -0300

    Delete src/app directory

--- src/app/models/placeholder.model.ts
+++
@@ -1,9 +0,0 @@
-export class Felipe{
-    public films: string;
-    public people: string;
-    public planets: string;
-    public species: string;
-    public starships: string; 
-    public vehicles: string;
-
-}
